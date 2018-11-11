b056beea40981961583b3043fb5ca37dafbe9fb1
oconorr3 <oconorr3@vt.edu>
2018-11-11 12:28:06 -0500

    Add props based event handlers for when header nav items are clicked. Add begginings of About Modal for when the about nav item is clicked.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,84 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-
-import HomePage from './Home.jsx';
+import Scene from './Scene.jsx';
+import Header from './Header.jsx';
 
 import '../styles/App.css';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showTextOverlay: true,
+      showAboutModal: false,
+      showProjectsModal: false,
+      showContactModal: false,
+    };
+  }
+
 
   componentDidCatch(error, info) {
     alert(`Unexpected Error Occured: ${error.message} \n\n refreshing page`);
     window.location.reload();
   }
 
+
+  //for when the home nav item is clicked
+  onHomeClicked = () => {
+    console.log('home clicked');
+    this.setState({
+      showTextOverlay: true,
+      showAboutModal: false,
+      showProjectsModal: false,
+      showContactModal: false,
+    });
+  }
+
+  //for when the About nav item is clicked
+  onAboutClicked = () => {
+    console.log('about clicked');
+    this.setState({
+      showTextOverlay: false,
+      showAboutModal: true,
+      showProjectsModal: false,
+      showContactModal: false,
+    });
+  }
+
+  //for when the Projects nav item is clicked
+  onProjectsClicked = () => {
+    console.log('projects clicked');
+    this.setState({
+      showTextOverlay: false,
+      showAboutModal: false,
+      showProjectsModal: true,
+      showContactModal: false,
+    });
+  }
+
+  //for when the Contact nav item is clicked
+  onContactClicked = () => {
+    console.log('contact clicked');
+    this.setState({
+      showTextOverlay: false,
+      showAboutModal: false,
+      showProjectsModal: false,
+      showContactModal: true,
+    });
+  }
+
   render() {
     return (
-      <div className="main-container">
-        <Router>
-          <Switch>
-            <Route exact path='/' component={HomePage}/>
-          </Switch>
-        </Router>
-      </div>
+        <div>
+          <Header
+            onHomeClicked={this.onHomeClicked}
+            onAboutClicked={this.onAboutClicked}
+            onProjectsClicked={this.onProjectsClicked}
+            onContactClicked={this.onContactClicked}
+            {...this.state}/>
+          <Scene/>
+        </div>
     );
   }
+
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,55 +9,9 @@ export default class Header extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      activeItemIndex: 0
-      /*homeActive: true,
-      aboutActive: false,
-      projectsActive: false,
-      contactActive: false,*/
-    };
-  }
-
-  //Handler for when a navItem is clicked
-  onNavItemClicked = (clickedItemIndex, clickedMenuItem) => {
-    console.log('SELECTED '  + clickedMenuItem + '\n eventKey ' + clickedItemIndex);
-    this.setState({ activeItemIndex : clickedItemIndex });
-
-    /*//this has to be done a better way
-    if (this.state.activeItemIndex == 0) {
-      this.setState({
-        homeActive: true,
-        aboutActive: false,
-        projectsActive: false,
-        contactActive: false,
-      });
-    } else if (this.state.activeItemIndex == 1) {
-      this.setState({
-        homeActive: false,
-        aboutActive: true,
-        projectsActive: false,
-        contactActive: false,
-      });
-    } else if (this.state.activeItemIndex == 2) {
-      this.setState({
-        homeActive: false,
-        aboutActive: false,
-        projectsActive: true,
-        contactActive: false,
-      });
-    } else {
-      this.setState({
-        homeActive: false,
-        aboutActive: false,
-        projectsActive: false,
-        contactActive: true,
-      });
-    }*/
-
   }
 
   render() {
-
     return (
       <Navbar id='header' fixedTop defaultExpanded>
         <Navbar.Header>
@@ -67,11 +21,11 @@ export default class Header extends React.Component {
            <Navbar.Toggle/>
          </Navbar.Header>
            <Navbar.Collapse>
-             <Nav bsStyle='pills' pullRight onSelect={i => this.onNavItemClicked(i)}>
-                <NavItem id='home' eventKey="0" className='nav-item' href='#home'>  Home     </NavItem>
-                <NavItem id='about' eventKey="1" className='nav-item' href='#about'>  About    </NavItem>
-                <NavItem id='projects' eventKey="2" className='nav-item' href='#projects'>  Projects </NavItem>
-                <NavItem id='contact'eventKey="3" className='nav-item' href='#contact'>  Contact   </NavItem>
+             <Nav bsStyle='pills' pullRight>
+                <NavItem className='nav-item' href='#home' onClick={this.props.onHomeClicked}>  Home     </NavItem>
+                <NavItem className='nav-item' href='#about' onClick={this.props.onAboutClicked}>  About    </NavItem>
+                <NavItem className='nav-item' href='#projects' onClick={this.props.onProjectsClicked}>  Projects </NavItem>
+                <NavItem className='nav-item' href='#contact' onClick={this.props.onContactClicked}>  Contact   </NavItem>
               </Nav>
             </Navbar.Collapse>
         </Navbar>
